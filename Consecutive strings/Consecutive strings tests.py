d7af9a279eb6823b606033dc09888e186974baeb
David <dave.pylypenko@gmail.com>
2022-04-20 16:50:16 +0300

    update Consecutive strings folder

---
+++ Consecutive strings/Consecutive strings tests.py
@@ -0,0 +1,9 @@
+
+longest_consec(["zone", "abigail", "theta", "form", "libe", "zas"], 2)
+longest_consec(["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"], 1)
+longest_consec([], 3)
+longest_consec(["itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"], 2)
+longest_consec(["zone", "abigail", "theta", "form", "libe", "zas"], -2)
+longest_consec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 3)
+longest_consec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 15)
+longest_consec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 0)
