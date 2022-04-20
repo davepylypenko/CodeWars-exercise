153500cc038e77404f6a057c6d96ddd2dd111ed9
David <dave.pylypenko@gmail.com>
2022-04-20 16:58:53 +0300

    update Prime Numbers folder

---
+++ Prime Numbers/Prime numbers tests.py
@@ -0,0 +1,25 @@
+is_prime(0)
+is_prime(1)
+is_prime(2)
+is_prime(73)
+is_prime(75)
+is_prime(-1)
+
+
+"""return True"""
+is_prime(3)
+is_prime(5)
+is_prime(7)
+is_prime(41)
+is_prime(5099)
+
+
+"""return False"""
+is_prime(4)
+is_prime(6)
+is_prime(8)
+is_prime(9)
+is_prime(45)
+is_prime(-5)
+is_prime(-8)
+is_prime(-41)
