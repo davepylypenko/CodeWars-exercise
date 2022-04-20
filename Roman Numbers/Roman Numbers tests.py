dfe479b31809825a4cd8c2d5ba472f22b31d45b6
David <dave.pylypenko@gmail.com>
2022-04-20 16:23:46 +0300

    update Roman Numbers folder

---
+++ Roman Numbers/Roman Numbers tests.py
@@ -0,0 +1,15 @@
+"""to roman"""
+
+RomanNumerals.toRoman(1000)
+RomanNumerals.toRoman(4)
+RomanNumerals.toRoman(1)
+RomanNumerals.toRoman(1990)
+RomanNumerals.toRoman(2008)
+
+"""to arabic"""
+
+RomanNumerals.fromRoman('XXI')
+RomanNumerals.fromRoman('I')
+RomanNumerals.fromRoman('IV')
+RomanNumerals.fromRoman('MMVIII')
+RomanNumerals.fromRoman('MDCLXVI')
